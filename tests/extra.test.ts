5cace9994708391c8fde79d2c7b78b064697f47c
Sean Z <contact@seanzeng.com>
2020-03-03 07:56:46 +1300

    added more tests

--- tests/extra.test.ts
+++ tests/extra.test.ts
@@ -28,3 +28,20 @@ test('empty string valid', () => {
     expect(checkHTML(input)).toMatch(output);
 });
 
+test('massive listings', () => {
+    const input = '<A><B><C><D></D></C></B></A>';
+    const output = 'Correctly tagged paragraph';
+
+    expect(checkHTML(input)).toMatch(output);
+
+    const input1 = '<A><B></B><C></C></A>';
+    const output1 = 'Correctly tagged paragraph';
+
+    expect(checkHTML(input1)).toMatch(output1);
+
+    const input2 = '<A><B><C></C></B></A>';
+    const output2 = 'Correctly tagged paragraph';
+
+    expect(checkHTML(input2)).toMatch(output2);
+
+});
